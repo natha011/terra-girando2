f12244ef44a4817a304a65bad803f3c9cb3894f9
nathan da 160 <nathan.alves.silva@escola.pr.gov.br>
2026-03-18 15:32:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
-    createCanvas(1920, 1080,WEBGL);//plano 3D
-    terra=loadImage('terrajpg')
+    createCanvas(1920,1080, WEBGL);//plano 3D
+    terra=loadImage('terra.jpg')
 }
 
 function draw() {
-background(205, 102, 94);//cor de fundo
-rotateY(millis()/1000)///rotação da terra
-Texture(terra);//texturada terra, imagem.
-Sphere (80, 100);//esfera 3D
+    background(205, 102, 94);//cor de fundo
+    rotateY(millis()/1000);//rotação da terra
+    texture(terra);//textura da terra, imagem.
+    sphere(80, 100);//esfera 3D
 }
